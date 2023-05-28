5719502ee9425bd8aa3f871a263c17ad3ceb9acf
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-28 14:34:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let prom1 = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve();
+	}, 1000)
+})
+let prom2 = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve();
+	}, 2000)
+})
+let prom3 = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve();
+	}, 3000)
+})
+let prom4 = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve();
+	}, 4000)
+})
+let prom5 = new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve();
+	}, 5000)
+})
+window.promises = [prom1, prom2, prom3, prom4, prom5];
+let x = Promise.any(promises);
+document.getElementById("output").value = x;
